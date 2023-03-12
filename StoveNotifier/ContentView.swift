1b598f3cf3c63b021a738e0bc0a921824147e49f
MotorBottle <motorbottt@gmail.com>
2023-03-12 20:05:03 +0800

    Initial Commit

---
+++ StoveNotifier/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StoveNotifier
+//
+//  Created by MotorBottle on 2023/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoveNotifier/StoveNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoveNotifierApp.swift
+//  StoveNotifier
+//
+//  Created by MotorBottle on 2023/3/12.
+//
+
+import SwiftUI
+
+@main
+struct StoveNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
